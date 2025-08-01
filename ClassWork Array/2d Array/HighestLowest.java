ac861cf6139546077e66ecab8ad0d5a227611d3d
Bustavo-coder <abdbasitegberongbe@gmail.com>
2025-08-01 06:24:06 +0100

    Hihest and smallest 2d array

--- ClassWork Array/2d Array/HighestLowest.java
+++ ClassWork Array/2d Array/HighestLowest.java
@@ -1,1 +1,44 @@
-public class
+import java.util.Arrays;
+public class HighestLowest{
+	public static void main (String [] args){
+		
+		int [][] values = {{70,60},{80,90},{100,50}};
+		//int largest =  values [0][0] ;
+		//int smallest =  values [0][0] ;
+		//int [] ranges = new int[2];
+		System.out.print(Arrays.toString(getHighest(values)));
+	
+
+
+	}
+
+
+public static int [] getHighest(int [] [] values){
+		int largest =  values[0][0];
+		int smallest =  values[0][0];
+	for(int count = 0; count < values.length; count++){
+		for(int counter = 0; counter < values[count].length; counter++){
+			if(values[count][counter] > largest) largest = values[count][counter];
+			if(values[count][counter] < smallest) smallest = values[count][counter];
+			
+		}
+	}
+
+	int [] highestSmallest  = {largest,smallest};
+	return highestSmallest;
+	
+	}
+		
+}
+
+
+
+
+
+//for(int number[]: values){
+		//for(int innerNumb : number){
+			//if(innerNumb > largest) largest = innerNumb;
+			//if(innerNumb  < smallest) smallest = innerNumb ;
+		//}
+			
+	//}
